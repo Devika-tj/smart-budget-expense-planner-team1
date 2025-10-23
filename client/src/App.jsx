d627b0b6e41df96aa478d38e9f1aeda7d17f3ae9
Gopika Prathap <gopika6august@gmail.com>
2025-10-23 23:56:14 +0530

    files updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
-import React from "react";
-import AdminDashboard from "./pages/AdminDashboard";
+import React from 'react'
+import './App.css'
+import Navbar from './pages/Navbar'
+import Home from './pages/Home'
+import Footer from './pages/Footer'
+import { Route, Routes } from 'react-router-dom'
 
-function App() {
-  return <AdminDashboard />;
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+     <Routes>
+        <Route path='/' element={<Home/>}></Route>
+      </Routes>
+      <Footer/>
+    </div>
+  )
 }
 
-export default App;
+export default App
