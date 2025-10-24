c279bff4691d793a4286b2a1d4254b4182c762da
vaishnav kv <vaishnavkv707@gmail.com>
2025-10-24 12:29:01 +0530

    Files updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import ExpenseDetails from "./pages/ExpenseDetails";
 import AdminDashboard from "./pages/AdminDashboard";
 import IncomePage from "./pages/IncomeDetails"
+import Sidebar from "./components/Sidebar";
+
 
 function App() {
   return (
     <div>
+      <Sidebar />
       <AdminDashboard />
       <ExpenseDetails />
       <IncomePage/>
+
     </div>
   );
 
